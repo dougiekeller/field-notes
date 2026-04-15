3f036ce0e39cf0499d0fb16dca10e75684d5602f
dougiekeller <dougie.keller@gmail.com>
2026-04-15 16:27:48 -0700

    Add files via upload

--- sw.js
+++ sw.js
@@ -1,5 +1,5 @@
 const CACHE_NAME = 'field-notes-v1';
-const ASSETS = ['./index.html'];
+const ASSETS = ['./index.html', './app.js'];
 
 self.addEventListener('install', e => {
   e.waitUntil(caches.open(CACHE_NAME).then(c => c.addAll(ASSETS)));
